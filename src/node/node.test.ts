b19c96d73c8d27d17fa3b1c1ee80e8542b5bc9df
zy0n <zy0n.barto@gmail.com>
2024-11-03 13:36:07 -0500

    setup testnode

--- src/node/node.test.ts
+++ src/node/node.test.ts
@@ -1,37 +1,32 @@
-import { beforeEach, describe, it } from "node:test";
-import { baseNode, webClientNode, type zkNode } from "./node.js";
+import {
+  //  beforeEach,
+  describe,
+  it,
+} from "node:test";
+import {
+  //   baseNode,
+  webClientNode,
+  // type zkNode
+} from "./node.js";
 import {
   generateNodeAddresses,
-  generatePrivateKey,
+  //   generatePrivateKey,
   getIPAddress,
 } from "./utils.js";
 // import { generateSeed } from "./utils.js";
 
 const testTopic = "test-topic";
-const mainSeed = new Uint8Array([
-  215, 24, 226, 44, 127, 255, 102, 250, 110, 148, 103, 62, 66, 82, 221, 139,
-  166, 170, 82, 204, 1, 100, 22, 169, 93, 50, 176, 43, 191, 16, 6, 72,
-]);
 
-const mainPrivateKey = await generatePrivateKey(mainSeed);
-
-const mainMultiaddrs: string[] = [];
+const mainMultiaddrs: string[] = [
+  "/ip4/54.167.24.167/tcp/8000/p2p/12D3KooWT19bnNKeLFPBHAtz1WhWrgiqJB76T6ikVwV4MCxtGjvd",
+  "/ip4/54.167.24.167/tcp/60000/ws/p2p/12D3KooWT19bnNKeLFPBHAtz1WhWrgiqJB76T6ikVwV4MCxtGjvd",
+];
 const currentIP = await getIPAddress();
 
 const currentAddresses = generateNodeAddresses([8000, 60000], currentIP);
 console.log("currentAddresses", currentAddresses);
 // const newAddresses: string[] = [];
 describe("Node", () => {
-  let zyNode: zkNode;
-  beforeEach(async () => {
-    zyNode = await baseNode(mainPrivateKey);
-    // no need to connect to bootstrap peers
-    mainMultiaddrs.push(...zyNode.getMultiaddrs());
-    zyNode.subscribe(testTopic, (data) => {
-      console.log("Node Received data: ", data);
-    });
-    zyNode.printPublicMultiaddrs();
-  });
   it("should be able to create a client node", async () => {
     // test goes here
     const client = await webClientNode(mainMultiaddrs);
@@ -43,15 +38,16 @@ describe("Node", () => {
     const data = {
       message: "Hello from client",
     };
-    const data2 = {
-      message: "Hello from client 2",
-    };
+    // const data2 = {
+    //   message: "Hello from client 2",
+    // };
     client.subscribe(testTopic, (data) => {
       console.log("Client Received data: ", data);
     });
     setInterval(() => {
+      console.log("sending data");
       client.send(testTopic, data);
-      zyNode.send(testTopic, data2);
+      //   zyNode.send(testTopic, data2);
     }, 1000);
   });
 });
