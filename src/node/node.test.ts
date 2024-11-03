eaf574002560d93a1855ca7305e5907d06c43832
zy0n <zy0n.barto@gmail.com>
2024-11-03 16:51:07 -0500

    gauntlet tests

--- src/node/node.test.ts
+++ src/node/node.test.ts
@@ -6,7 +6,8 @@ import {
 import {
   clientNode,
   //   baseNode,
-  webClientNode,
+  //   webClientNode,
+  type zkNode,
   // type zkNode
 } from "./node.js";
 import {
@@ -26,55 +27,114 @@ const currentIP = await getIPAddress();
 
 const currentAddresses = generateNodeAddresses([8000, 60000], currentIP);
 console.log("currentAddresses", currentAddresses);
+let nodeCount = 0;
+const spawnNode = async () => {
+  const node = await clientNode(mainMultiaddrs);
+  await node.connect();
+  console.log("Spawned node", nodeCount++);
+  node.subscribe(testTopic, (data) => {
+    console.log(`Client ${nodeCount} Received data: ${data}`);
+  });
+  node.printMultiaddrs();
+  return node;
+};
+
+const spawnChatter = async (node: zkNode, index: number) => {
+  const data = {
+    message: `Hello from client ${index}`,
+    timestamp: Date.now(),
+  };
+
+  const interval = setInterval(() => {
+    data.timestamp = Date.now();
+    node.send(testTopic, data);
+  }, 1000);
+
+  setTimeout(() => {
+    clearInterval(interval);
+    // node.stop();
+  }, 5000);
+};
+
+const killNodes = (nodes: zkNode[]) => {
+  nodes.forEach((node) => {
+    node.stop();
+  });
+};
+
+const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+const runSequence = async () => {
+  const nodes: zkNode[] = [];
+  const randomSeconds = 1000 * (Math.floor(Math.random() * 5) + 1);
+  for (let i = 0; i < 5; i++) {
+    spawnNode().then((node) => {
+      nodes.push(node);
+      spawnChatter(node, i);
+    });
+    await delay(randomSeconds);
+  }
+
+  setTimeout(() => {
+    killNodes(nodes);
+  }, 5000);
+};
+
+const runGauntlet = async () => {
+  for (let i = 0; i < 5; i++) {
+    runSequence();
+    await delay(10000);
+  }
+};
 
 describe("Node", () => {
   it("should be able to create a client node", async () => {
-    const client = await webClientNode(mainMultiaddrs);
-    await client.connect();
-    client.printMultiaddrs();
-    const da = client.getMultiaddrs();
-    console.log("da", da);
-    const data = {
-      message: "Hello from client",
-      timestamp: Date.now(),
-    };
-    client.subscribe(testTopic, (data) => {
-      console.log("Client Received data: ", data);
-    });
-    const interval = setInterval(() => {
-      data.timestamp = Date.now();
-      console.log("Sending data: ", data);
-      client.send(testTopic, data);
-    }, 1000);
+    await runGauntlet();
+    // const client = await webClientNode(mainMultiaddrs);
+    // await client.connect();
+    // client.printMultiaddrs();
+    // const da = client.getMultiaddrs();
+    // console.log("da", da);
+    // const data = {
+    //   message: "Hello from client",
+    //   timestamp: Date.now(),
+    // };
+    // client.subscribe(testTopic, (data) => {
+    //   console.log("Client Received data: ", data);
+    // });
+    // const interval = setInterval(() => {
+    //   data.timestamp = Date.now();
+    //   client.send(testTopic, data);
+    // }, 1000);
 
-    setTimeout(() => {
-      clearInterval(interval);
-      client.stop();
-    }, 5000);
+    // setTimeout(() => {
+    //   clearInterval(interval);
+    //   client.stop();
+    // }, 5000);
   });
 
-  it("should be able to create a second client node", async () => {
-    const client = await clientNode(mainMultiaddrs);
-    await client.connect();
-    client.printMultiaddrs();
-    const da = client.getMultiaddrs();
-    console.log("da", da);
-    const data = {
-      message: "Hello from Second client",
-      timestamp: Date.now(),
-    };
-    client.subscribe(testTopic, (data) => {
-      console.log("Second Client Received data: ", data);
-    });
-    const interval = setInterval(() => {
-      data.timestamp = Date.now();
-      console.log("Sending data: ", data);
-      client.send(testTopic, data);
-    }, 1000);
+  //   it("should be able to create a second client node", async () => {
+  //     const client = await clientNode(mainMultiaddrs);
+  //     await client.connect();
+  //     client.printMultiaddrs();
+  //     const da = client.getMultiaddrs();
+  //     console.log("da", da);
+  //     const data = {
+  //       message: "Hello from Second client",
+  //       timestamp: Date.now(),
+  //     };
+  //     client.subscribe(testTopic, (data) => {
+  //       console.log("Second Client Received data: ", data);
+  //     });
+  //     const interval = setInterval(() => {
+  //       data.timestamp = Date.now();
+  //       console.log("Sending data: ", data);
+  //       client.send(testTopic, data);
+  //     }, 1000);
 
-    setTimeout(() => {
-      clearInterval(interval);
-      client.stop();
-    }, 5000);
-  });
+  //     setTimeout(() => {
+  //       clearInterval(interval);
+  //       client.stop();
+  //     }, 5000);
+  //   });
 });
