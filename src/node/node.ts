e0a69878a11de884d6328178a15bb2a53064d394
zy0n <zy0n.barto@gmail.com>
2024-11-03 21:29:01 -0500

    connecting

--- src/node/node.ts
+++ src/node/node.ts
@@ -48,8 +48,8 @@ export class zkNode {
       );
     });
     this.libp2p.addEventListener("connection:close", async (evt) => {
-      const peer = await evt.detail.close();
-      console.log(`Connection to peer ${peer} closed`);
+      await evt.detail.close();
+      console.log(`Connection to peer ${evt.detail.remotePeer} closed`);
     });
   };
 
